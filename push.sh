82536e105c28130dcccb77d261f83f93188ede9b
inaimathi <inaimathi@self>
2014-05-29 09:35:53 -0400

    -langnostic.base is now transferred normally (may want to change back to append later, but I'll be doing a lot of history re-writing first)

--- push.sh
+++ push.sh
@@ -1,3 +1,2 @@
-rsync *.lisp *.asd --progress --exclude=blog-chronology.lisp inaimathi.ca:quicklisp/local-projects/langnostic/
-rsync langnostic.base --progress --append-verify inaimathi.ca:quicklisp/local-projects/langnostic/
+rsync *.lisp *.asd langnostic.base --progress --exclude=blog-chronology.lisp inaimathi.ca:quicklisp/local-projects/langnostic/
 rsync static/img/* --progress inaimathi.ca:quicklisp/local-projects/langnostic/static/img/
