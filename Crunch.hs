9b77edae65bc9402c680e017152cb7b66fd7689c
inaimathi <inaimathi@self>
2015-08-09 01:02:15 -0400

    - Word-smithing on distribution.md - Added first draft of Crunch.hs

---
+++ Crunch.hs
@@ -0,0 +1,28 @@
+module Main where
+
+import Data.List
+import Data.Function
+import Data.Char
+import System.Environment
+import Control.Monad
+
+crunch :: String -> [(String, Int)]
+crunch s = filterCommons . filterOneOfs . frequencyMap $ map toLower s
+    where filterOneOfs = filter ((>2) . snd)
+          frequencyMap = map (\w -> (head w, length w)) . group . sort . words
+          filterCommons = filter (not . (`elem` commonWords) . fst)
+          commonWords = ["the", "of", "to", "a", "is", "and"
+                        , "if", "it", "i", "as", "but", "an"
+                        , "are", "at", "in", "than", "you"]
+
+concatMapM :: (Monad m) => (a -> m [b]) -> [a] -> m [b]
+concatMapM f xs   =  liftM concat (mapM f xs)
+
+main :: IO ()
+main = do
+  [fname] <- getArgs
+  f <- readFile fname
+  mapM_ p . byCount $ crunch f
+  return ()
+      where byCount = sortBy (flip compare `on` snd)
+            p (str, ct) = putStrLn $ concat [str, " - ", show ct]
