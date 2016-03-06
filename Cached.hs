b7f4b29e91e07065da01d697b8d4451c1e970948
inaimathi <leo.zovic@gmail.com>
2016-03-06 11:59:17 -0500

    Working cache system and posts
    - Working file caching mechanism (fixed bug where it would stat disk on
      every request after the initial delay is up; this is also a bug with
      the current live Langnostic)
    - First cut of encoded post cache

--- Cached.hs
+++ Cached.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Cached (readCache, newCache, minutes, hours) where
+module Cached (Cache, readCache, newCache, minutes, hours) where
 
 import System.Time
+import System.Posix.Files
 import Data.IORef
 import Control.Monad.IO.Class (liftIO)
+import Data.Ratio
 
 data Cached a = Cached { reader :: (FilePath -> IO a)
                        , file :: FilePath
@@ -20,9 +22,22 @@ readCache cache = do
   let diff = now `diffClockTimes` (lastChecked c)
   if (cacheLimit c) >= diff
   then return $ value c
-  else do newVal <- (reader c) (file c)
-          _ <- writeIORef cache $ c { value = newVal, lastChecked = now }
-          return $ newVal
+  else do stat <- getFileStatus (file c)
+          if (lastChecked c) > (epochToClockTime $ modificationTime stat)
+          then do _ <- writeIORef cache $ c { lastChecked = now }
+                  return $ value c
+          else do newVal <- (reader c) (file c)
+                  _ <- writeIORef cache $ c { value = newVal, lastChecked = now }
+                  return $ newVal
+
+
+epochToClockTime :: Real a => a -> ClockTime
+epochToClockTime x =
+    TOD seconds secfrac
+    where ratval = toRational x
+          seconds = floor ratval
+          secfrac = floor $ (ratval - (seconds % 1) ) * picosecondfactor
+          picosecondfactor = 10 ^ 12
 
 newCache :: TimeDiff -> FilePath -> (FilePath -> IO a) -> IO (Cache a)
 newCache cacheLimit fname reader = do
---
+++ Post.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Post where
+
+import Text.Pandoc
+import Text.Pandoc.Walk (walk)
+import Text.Blaze.Html (Html)
+
+import Cached
+
+cachePost :: FilePath -> IO (Cache Html)
+cachePost fpath = newCache (minutes 30) fpath readPost
+
+readPost :: FilePath -> IO Html
+readPost fpath = do
+  f <- readFile fpath
+  return $ case readMarkdown def f of
+    Right p -> writeHtml def . footnotes $ walk linkedHeaders p
+    _ -> error $ "Invalid post: " ++ fpath
+
+----------
+-- Transform headers into mid-post anchors
+linkedHeaders :: Block -> Block
+linkedHeaders (Header n opts@(slug,_,_) content) = Header n opts linked
+    where linked = (anchor slug) ++ [ Link ("",[],[]) content ("#" ++ slug, "") ]
+linkedHeaders node = node
+
+----------
+-- Transform footnote "links" into footnotes
+footnotes :: Pandoc -> Pandoc
+footnotes doc = Pandoc meta $ content ++ compileFootnotes tbl
+    where tbl = flip zip [1..] $ queryWith extractFootnotes doc
+          Pandoc meta content = walk (replaceFootnote tbl) doc
+
+extractFootnotes :: Inline -> [Inline]
+extractFootnotes lnk@(Link _ _ ("footnote",_)) = [lnk]
+extractFootnotes _ = []
+
+replaceFootnote :: [(Inline, Int)] -> Inline -> Inline
+replaceFootnote tbl elem@(Link _ content _) =
+    case lookup elem tbl of
+      Nothing -> elem
+      Just n ->
+          Link ("", [], []) newContents ("#foot-" ++ show n, "")
+              where newContents = concat [anchor ("note-" ++ show n), [Str $ concat ["[", show n, "]"]]]
+replaceFootnote _ elem = elem
+
+compileFootnotes :: [(Inline, Int)] -> [Block]
+compileFootnotes fs = [ HorizontalRule
+                      , Header 3 ("", [], []) [Str "Footnotes"]
+                      , BulletList $ map toFootList fs]
+    where toFootList ((Link _ content _), n) = [Para $ concat [ anchor ("foot-" ++ show n)
+                                                              , [Link ("", [], []) [Str "[Back]"] ("#note-" ++ show n, "")]
+                                                              , content]]
+
+anchor :: String -> [Inline]
+anchor name = [ RawInline (Format "html") ("<a name=\"" ++ name ++ "\">")
+              , RawInline (Format "html") "</a>"]
+
+replaceFootnotes :: Inline -> Inline
+replaceFootnotes (Link _ _ ("footnote",_)) = Str ""
