91ab3bb803288a098f5134270431b3c4f1690951
inaimathi <leo.zovic@gmail.com>
2016-03-20 23:27:30 -0400

    Generalized Cached and CacheMap

--- Cached.hs
+++ Cached.hs
@@ -4,31 +4,23 @@ module Cached ( Cache, readCache, newCache
               , minutes, hours ) where
 
 import System.Time
-import System.Directory
-import System.Posix.Files
 import Data.IORef
-import Control.Monad.IO.Class (liftIO)
 import Data.Ratio
-
-import Control.Monad
-
 import Data.Maybe (fromJust)
-
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Control.Monad.IO.Class (liftIO)
 
-data Cached a = Cached { reader :: (FilePath -> IO a)
-                       , file :: FilePath
+data Cached a = Cached { cachedHandle :: IO a
                        , cacheLimit :: TimeDiff
                        , lastChecked :: Maybe ClockTime
                        , value :: Maybe a }
 
 type Cache a = IORef (Cached a)
 
-newCache :: TimeDiff -> (FilePath -> IO a) -> FilePath -> IO (Cache a)
-newCache cacheLimit reader fname = do
-  val <- reader fname
-  newIORef $ Cached { reader = reader, file = fname, cacheLimit = cacheLimit, value = Nothing, lastChecked = Nothing }
+newCache :: TimeDiff -> IO a -> IO (Cache a)
+newCache limit handle = do
+  newIORef $ Cached { cachedHandle = handle, cacheLimit = limit, value = Nothing, lastChecked = Nothing }
 
 readCache :: Cache a -> IO a
 readCache cache = do
@@ -39,14 +31,7 @@ readCache cache = do
     Just last -> let diff = now `diffClockTimes` last
                  in if (cacheLimit c) >= diff
                     then return . fromJust $ value c
-                    else do stat <- getFileStatus (file c)
-                            if last > (epochToClockTime $ modificationTime stat)
-                            then do _ <- writeIORef cache $ bumpTime now c
-                                    return . fromJust $ value c
-                            else readNew cache c now
-
-cacheValue :: Cached a -> IO a
-cacheValue c = (reader c) (file c)
+                    else readNew cache c now
 
 bumpTime :: ClockTime -> Cached a -> Cached a
 bumpTime now c = c { lastChecked = Just now }
@@ -56,30 +41,30 @@ bumpValue v c = c { value = Just v }
 
 readNew :: Cache a -> Cached a -> ClockTime -> IO a
 readNew cache c now = do
-  newVal <- cacheValue c
+  newVal <- cachedHandle c
   _ <- writeIORef cache . bumpValue newVal $ bumpTime now c
   return $ newVal
 
 ---------- Cache Map infrastructure
-data CacheMap a = CacheMap { fn :: (FilePath -> IO a)
-                   , ref :: IORef (Map FilePath (Cache a)) }
+data CacheMap a b = CacheMap { mapHandle :: (b -> IO a)
+                             , ref :: IORef (Map b (Cache a)) }
 
-newCacheMap :: (FilePath -> IO a) -> IO (CacheMap a)
-newCacheMap f = do
+newCacheMap :: (b -> IO a) -> IO (CacheMap a b)
+newCacheMap n = do
   r <- newIORef $ Map.empty
-  return $ CacheMap { fn = f, ref = r }
+  return $ CacheMap { mapHandle = n, ref = r }
 
-insert :: CacheMap a -> TimeDiff -> FilePath -> IO (Cache a)
-insert cacheMap diff fname = do
+insert :: Ord b => CacheMap a b -> TimeDiff -> b -> IO (Cache a)
+insert cacheMap diff name = do
   m <- readIORef (ref cacheMap)
-  c <- newCache diff (fn cacheMap) fname
-  _ <- writeIORef (ref cacheMap) $ Map.insert fname c m
+  c <- newCache diff $ (mapHandle cacheMap) name
+  _ <- writeIORef (ref cacheMap) $ Map.insert name c m
   return c
 
-lookup :: CacheMap a -> FilePath -> IO (Maybe a)
-lookup cacheMap fname = do
+lookup :: Ord b => CacheMap a b -> b -> IO (Maybe a)
+lookup cacheMap name = do
   m <- readIORef $ ref cacheMap
-  case Map.lookup fname m of
+  case Map.lookup name m of
     Nothing -> return $ Nothing
     Just looked -> do c <- readCache looked
                       return $ Just c
