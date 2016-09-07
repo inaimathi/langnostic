3abfecf86131ebed2c390ce7a79fd5fa7d048f24
inaimathi <leo.zovic@gmail.com>
2016-09-07 15:01:39 -0400

    Fix push.sh
    - Fix push.sh; it now uses `scp` to push up new posts, and only uses
      `rsync` to generate the diff list (this properly triggers fs signals
      on the remote end)
    - Remove really old, unused bitching post

--- push.sh
+++ push.sh
@@ -1,1 +1,2 @@
-rsync -r resources project.clj src test doc LICENSE --exclude=*~ --progress inaimathi.ca:langnostic
+rsync -ric resources project.clj src test doc LICENSE --exclude=*~ --exclude=resources/posts/* --progress inaimathi.ca:langnostic
+rsync -ric resources/posts/ --exclude=*~ --dry-run --progress inaimathi.ca:langnostic/resources/posts/ | cut -d ' ' -f2 | tail -n +2 | xargs -I+ scp "resources/posts/+" inaimathi.ca:langnostic/resources/posts/
