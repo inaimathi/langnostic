0c6e8a2fa56c8365e39d5489466ee677258dfc69
inaimathi <leo.zovic@gmail.com>
2016-12-28 09:41:02 -0500

    Fix css and push.sh
    - Correct spelling mistake in langnostic.css body rule
    - push.sh now also syncs up resources/public

--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
 rsync -r project.clj src test doc LICENSE --exclude=*~ --progress inaimathi.ca:langnostic
 rsync -rc resources/posts --exclude=*~ --progress inaimathi.ca:langnostic/resources
 rsync resources/posts.json --progress inaimathi.ca:langnostic/resources
+rsync -r --progress resources/public/ inaimathi.ca:langnostic/resources/public/
