c79d699c2f59af6d543818c2dc57628db573bf1e
inaimathi <leo.zovic@gmail.com>
2020-01-09 20:16:42 -0500

    Bump version and exclude clj files but include target

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject langnostic "0.1.0-SNAPSHOT"
+(defproject langnostic "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Expat License"
