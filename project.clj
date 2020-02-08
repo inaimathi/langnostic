b94fc4678835f407c89179a7a6e70fb18bdc2e4c
inaimathi <leo.zovic@gmail.com>
2020-02-07 23:36:41 -0500

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject langnostic "0.1.2-SNAPSHOT"
+(defproject langnostic "0.1.3-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Expat License"
