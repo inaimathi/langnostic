29719459117ace7b71573668ef0188d7ec52750c
inaimathi <leo.zovic@gmail.com>
2023-05-25 18:56:40 -0400

    push script changes

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
-rsync -r project.clj src test doc target LICENSE --exclude=*~ --exclude=*clj --progress inaimathi.ca:langnostic
-rsync -rc resources/posts --exclude=*~ --progress inaimathi.ca:langnostic/resources
-rsync resources/posts.json --progress inaimathi.ca:langnostic/resources
-rsync -r --progress resources/public/ inaimathi.ca:langnostic/resources/public/
+rsync -r project.clj src test doc target LICENSE --exclude=*~ --exclude=*clj --progress langnostic.inaimathi.ca:langnostic
+rsync -rc resources/posts --exclude=*~ --progress langnostic.inaimathi.ca:langnostic/resources
+rsync resources/posts.json --progress langnostic.inaimathi.ca:langnostic/resources
+rsync -r --progress resources/public/ langnostic.inaimathi.ca:langnostic/resources/public/
