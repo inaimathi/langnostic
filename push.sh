60d4963f78a458912f3903d87cf065bdfde4e6cc
inaimathi <leo.zovic@gmail.com>
2026-07-16 20:36:35 -0400

    arbitrary update tweaks

--- push.sh
+++ push.sh
@@ -1,4 +1,3 @@
-rsync -r project.clj src doc LICENSE --exclude=*~ --exclude=*clj --progress langnostic.inaimathi.ca:langnostic
-rsync -rc resources/posts --exclude=*~ --progress langnostic.inaimathi.ca:langnostic/resources
-rsync resources/posts.json --progress langnostic.inaimathi.ca:langnostic/resources
-rsync -r --progress resources/public/ langnostic.inaimathi.ca:langnostic/resources/public/
+rsync -rc resources/posts --exclude=*~ --progress inaimathi.ca:langnostic/resources
+rsync resources/posts.json --progress inaimathi.ca:langnostic/resources
+rsync -r --exclude=*~ --progress resources/public/ inaimathi.ca:langnostic/resources/public/
